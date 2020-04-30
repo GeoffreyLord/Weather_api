e82f7106c7c14e44ff460a8292a7ef6381ed1b08
GeoffreyLord <Geoffrey.Lord.Jr@gmail.com>
2020-04-29 22:26:28 -0600

    Finished Parsing Code and Implementing the GPS Coords User Input

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Weather_api
-First time using python requests and json libraries. This is a simple demo that will print out the weather in a given area based off your lat and long. 
+First time using the python requests libraty. This is a simple demo that will print out the weather in a given area based off your lat and long.
+This code will ask for GPS coords, call the weather.gov api service to get gridpoints of your location. After getting your gridpoints it will call another weather.gov api to get hourly weather data. That weather data is then parsed and printed.  
+ 
--- weather_api.py
+++ weather_api.py
@@ -5,31 +5,54 @@
 
 import requests
 import json
-
+import time
 #Link Below was able to provide list of other API's. One was weather
-#weather = requests.get("https://api.weather.gov/points/39.7348,-104.9653")
+#User Input
+lat = input("Enter latitude (dec): ")
+long = input("Enter longitude (dec): ")
+url = "https://api.weather.gov/points/" + lat + "," + long
 
-#This Request will get the weather data in JSON format 
-#To get gridpoints enter in your coords to this api
-#https://api.weather.gov/points/{latitude},{longitude}
+#API Request and Parsing for Grid Points
+grid = (requests.get(url)).text
+grid_index = (grid.index("/forecast"))
+x = grid[grid_index - 2] + grid[grid_index - 1]
+y = grid[grid_index - 5] + grid[grid_index - 4]
 
-#20 20
-#44 56
-weather = requests.get("https://api.weather.gov/gridpoints/BOU/44,56/forecast")
 
-#Want data from lines 66-77 
+#This Request will get the hourly weather data in JSON format 
+weather = requests.get("https://api.weather.gov/gridpoints/BOU/" + x + ',' + y + "/forecast/hourly")
 weather = weather.text
-#weather = json.loads(weather)
-print(weather)
-#Parse Data 
-start_ind = weather.index("name")
-end_ind = weather.index("mph.")
-#print(start_ind,end_ind)
+
+#Parse Data
+#Get indecies from current hour 
+start_ind = weather.index('"number": 1,')
+end_ind = weather.index('"icon": "https://api.weather.gov/icons/land/night/skc?size=small",')
+
 
 current_weather = [None]*((end_ind-start_ind)+4)
 for i in range(start_ind,end_ind+4):
 	current_weather[i-start_ind] = weather[i]
 current_weather = ''.join(current_weather)
+
+#Parsing Current Weather Into Clean Output
+temp_index = current_weather.index('ture":')
+temp = current_weather[temp_index + 7] + current_weather[temp_index + 8]
+windspeed_ind = current_weather.index('Speed": "')
+wind_speed = current_weather[windspeed_ind + 9] + ' mph'
+winddirect_ind = current_weather.index('Direction": "')
+wind_direction = current_weather[winddirect_ind + 13] + current_weather[winddirect_ind + 14] + current_weather[winddirect_ind + 15]
+
+#Get Local Time
+localtime = time.asctime( time.localtime(time.time()) )
+#Print Weather at Current Time/Location
 print('\n'*3)
-print(current_weather)
-print('\n'*3)
+print('\tCURRENT WEATHER\t -- LAT: ' + lat +  ", LONG: " + long)
+print(' ')
+print("\tTime: " +  localtime)
+print("\tTemperature: " + temp + "F")
+print("\tWind Speed: " + wind_speed) 
+print("\tWind Direction: " + wind_direction)
+print('\n')
+
+
+
